75255981df38ced6fecdca39e65e845009d9748a
Gabriel Torres <al2012.163.torres@gmail.com>
2021-12-15 05:49:46 -0300

    fix: language consistency

--- src/Pages/NoRoute.jsx
+++ src/Pages/NoRoute.jsx
@@ -4,9 +4,9 @@ import { Link } from 'react-router-dom';
 export default function NoRoute() {
   return (
     <div>
-      <h1>404 - Page not found</h1>
+      <h1>404 - Página não encontrada</h1>
       <Link to="/">
-        <button>Click here to go back to the home page</button>
+        <button>Voltar para o início</button>
       </Link>
     </div>
   );
