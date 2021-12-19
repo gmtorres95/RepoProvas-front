33066963e566427405001a76634cf467e1df307f
Gabriel <al2012.163.torres@gmail.com>
2021-12-19 10:50:55 -0300

    feat: implement route /list/teachers

--- src/pages/ListByDiscipline.jsx
+++ src/pages/ListByDiscipline.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-export default function Exams() {
-  return(
+export default function ListByDiscipline() {
+  return (
     <div>
       <h1>Essa página será implementada em breve</h1>
       <Link to="/">
