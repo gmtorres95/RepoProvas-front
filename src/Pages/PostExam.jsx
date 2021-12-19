cde019a8f86614efe696699aad3971d90720a3a0
Gabriel <al2012.163.torres@gmail.com>
2021-12-19 02:34:19 -0300

    chore: implement teacher selection

--- src/Pages/PostExam.jsx
+++ src/Pages/PostExam.jsx
@@ -3,38 +3,66 @@ import { getCategories } from '../services/categoriesService';
 import { getDisciplines } from '../services/disciplinesService';
 
 export default function PostExam() {
-  const [categories, setCategories] = useState([{ category: 'Carregando...' }]);
+  const initialState = [{
+    id: 0,
+    category: 'Carregando...',
+    discipline: 'Carregando...',
+    name: 'Selecione uma disciplina',
+  }];
+  const [categories, setCategories] = useState(initialState);
   const [selectedCategoryId, setSelectedCategoryId] = useState(0);
-  const [disciplines, setDisciplines] = useState([{ category: 'Carregando...' }]);
+  const [disciplines, setDisciplines] = useState(initialState);
   const [selectedDisciplineId, setSelectedDisciplineId] = useState(0);
+  const [teachers, setTeachers] = useState(initialState);
+  const [selectedTeacherId, setSelectedTeacherId] = useState(0);
 
   useEffect(() => {
     getCategories(setCategories);
     getDisciplines(setDisciplines);
   }, []);
 
+  function selectDiscipline(disciplineId) {
+    setSelectedDisciplineId(disciplineId);
+    setSelectedTeacherId(0);
+    if (disciplineId === 0) setTeachers(initialState);
+    else {
+      disciplineId = disciplines.map((discipline) => discipline.id).indexOf(Number(disciplineId));
+      setTeachers(disciplines[disciplineId].teachers);
+    }
+  }
+
   return(
     <div>
       <h1>Postar prova</h1>
       <form>
         <label for="category">Selecione a categoria da prova: </label>
-        <select name="category" id="category" onChange={(e) => setSelectedCategoryId(e.target.value)}>
+        <select name="category" id="category" onChange={(e) => setSelectedCategoryId(Number(e.target.value))}>
           <option value={0} />
-            {categories.map((category) => 
-              <option value={category.id} key={category.id}>
-                {category.category}
-              </option>
+          {categories.map((category) => 
+            <option value={category.id} key={category.id}>
+              {category.category}
+            </option>
             )}
         </select>
 
-        <label for="disciplines">Selecione a disciplina: </label>
-        <select name="disciplines" id="disciplines" onChange={(e) => setSelectedDisciplineId(e.target.value)}>
+        <label for="discipline">Selecione a disciplina: </label>
+        <select name="discipline" id="discipline" onChange={(e) => selectDiscipline(Number(e.target.value))}>
           <option value={0} />
-            {disciplines.map((discipline) => 
-              <option value={discipline.id} key={discipline.id}>
-                {discipline.discipline}
-              </option>
-            )}
+          {disciplines.map((discipline) =>
+            <option value={discipline.id} key={discipline.id}>
+              {discipline.discipline}
+            </option>
+          )}
+        </select>
+
+        <label for="teacher">Selecione o professor: </label>
+        <select name="teacher" id="teacher" onChange={(e) => setSelectedTeacherId(Number(e.target.value))}>
+          <option value={0} />
+          {teachers.map((teacher) =>
+            <option value={teacher.id} key={teacher.id}>
+              {teacher.name}
+            </option>
+          )}
         </select>
       </form>
     </div>
--- src/services/disciplinesService.js
+++ src/services/disciplinesService.js
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
-function getDisciplines(setDiscipline) {
+function getDisciplines(setDisciplines) {
   axios.get(`${process.env.REACT_APP_API_BASE_URL}/disciplines`)
     .then((resp) => {
-      setDiscipline(resp.data);
+      setDisciplines(resp.data);
     })
     .catch((err) => {
       alert('Erro ao buscar disciplinas');
