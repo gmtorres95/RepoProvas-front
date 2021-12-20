627a26f64016167981892d3de63709b7e38997a6
Gabriel <al2012.163.torres@gmail.com>
2021-12-20 06:40:17 -0300

    feat: implement home button

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,16 @@
 import React from 'react';
 import { Outlet } from 'react-router';
+import { Link, useLocation } from 'react-router-dom';
 
 export default function App() {
+  const location = useLocation();
+
   return (
-    <Outlet />
+    <div>
+      {location.pathname !== '/' && <Link to='/'>
+        <button>Voltar para o início</button>
+      </Link>}
+      <Outlet />
+    </div>
   );
 }
